adabd0da2187c8d926a89cda4dcaa141ca1f18d6
shivani <shivanicharhate03@gmail.com>
2023-09-12 23:35:51 +0530

    first commit

---
+++ src/Component/Square.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Square = (props) =>{
+
+    return (
+        <div
+        onClick = {props.onClick}
+        style={{
+            border:"1px solid", height:"100px",width:"100%",display:"flex",justifyContent:"center",alignItems:"center"
+        }}>
+            <h5>{props.value}</h5>
+        </div>
+    );
+}
+export default Square;
